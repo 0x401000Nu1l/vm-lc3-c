d214d6a497199368b73836b6bfc66d64d210d9b1
naw0 <xindu1106@gmail.com>
2022-10-04 23:48:34 +0800

    完成部分指令

--- vm-lc3-c.c
+++ vm-lc3-c.c
@@ -117,7 +117,7 @@ uint16_t sign_extend(uint16_t x,int bit_count)
 
 uint16_t swap16(uint16_t x) 
 {
-    return (x<<8)|(x>>8)
+    return (x<<8)|(x>>8);
 }
 
 void update_flags(uint16_t r) 
@@ -183,3 +183,67 @@ uint16_t mem_read(uint16_t address)
     }
     return memory[address];
 } 
+
+int main(int argc,const char* argv[])
+{
+    if(argc < 2)
+    {
+        printf("lc3 [image-file1] ... \n");
+        exit(2);
+    }
+    for (int j = 1;j<argc;++j){
+        if (!read_image(argv[j]))
+        {
+            printf("failed to load image:%S \n",argv[j]);
+            exit(1);
+        }
+    }
+    signal(SIGINT,handle_interrupt);
+    disable_input_buffering();
+
+    reg[R_COND] = FL_ZRO;
+
+    enum {PC_START = 0x3000};
+    reg[R_PC] = PC_START;
+
+    int running = 1;
+    while(running) 
+    {
+        uint16_t instr = mem_read(reg[R_PC]++);
+        uint16_t op = instr >>12;
+        switch(op)
+        {
+            case OP_ADD:
+            {
+                uint16_t r0 = (instr >> 9 ) &0x7;
+                uint16_t r1 = (instr >> 6 ) &0x7;
+                uint16_t imm_flag = (instr >> 5) & 0x1;
+                if (imm_flag){
+                    uint16_t imm5 = sign_extend(instr &0x1F,5);
+                    reg[r0] = reg[r1] +imm5;
+                }
+                else{
+                    uint16_t r2 = instr & 0x7 ;
+                    reg[r0] = reg[r1] +reg[r2];
+                }
+                update_flags(r0);  
+            }
+            break;
+            case OP_AND:
+            {
+                uint16_t r0 = (instr >> 9 ) &0x7;
+                uint16_t r1 = (instr >> 6 ) &0x7;
+                uint16_t imm_flag = (instr >> 5) & 0x1;
+                if (imm_flag){
+                    uint16_t imm5 = sign_extend(instr &0x1F,5);
+                    reg[r0] = reg[r1] & imm5;
+                }
+                else{
+                    uint16_t r2 = instr & 0x7 ;
+                    reg[r0] = reg[r1] & reg[r2];
+                }
+                update_flags(r0); 
+            }
+        }
+    }
+}
