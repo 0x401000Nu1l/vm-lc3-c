8c7f6bd522a8ac6d2389b9de69bde2b8bfa186f2
naw0 <xindu1106@gmail.com>
2022-10-04 17:43:40 +0800

    完成虚拟化CPU

---
+++ vm-lc3-c.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, // program counter
+    
+    R_COND,
+    R_COUNT
+};
